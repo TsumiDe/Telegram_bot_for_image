76afb9cf683e4ba8907a534f11f828790a6d697d
TsumiDe <m.m.abibulayev@student.khai.edu>
2023-02-12 20:51:07 +0100

    RemoveBackground, stage 1

---
+++ src/main/java/org/bot/telegramappbot/TelegramAppBotApplication.java
@@ -0,0 +1,12 @@
+package org.bot.telegramappbot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAppBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramAppBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bot/telegramappbot/TelegramAppBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bot.telegramappbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAppBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
